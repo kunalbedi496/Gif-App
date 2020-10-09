fd986c92bc7116ef7a3e92fc832983df47c0c5bb
kunalbedi496 <kunalbedi496@gmail.com>
2020-10-09 15:39:34 +0530

    Updates

---
+++ precache-manifest.74e073da88e2e85f5d9c86a27ac8aa80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6e4c7521361749a86c51bbdcc4375a",
+    "url": "/Gif-App/index.html"
+  },
+  {
+    "revision": "dca17ccfd81e9da959cb",
+    "url": "/Gif-App/static/css/main.23bffde2.chunk.css"
+  },
+  {
+    "revision": "42c0a672e70f7501a7fc",
+    "url": "/Gif-App/static/js/2.692db0e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gif-App/static/js/2.692db0e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca17ccfd81e9da959cb",
+    "url": "/Gif-App/static/js/main.3abca1f0.chunk.js"
+  },
+  {
+    "revision": "be62727880e1fd73fd38",
+    "url": "/Gif-App/static/js/runtime-main.471079fc.js"
+  }
+]);
